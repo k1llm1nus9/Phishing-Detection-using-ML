5d9ceaada2cac01edaf7974d58704ef01114c98d
shivamverma <vermasmash@gmail.com>
2020-03-19 15:44:15 +0530

    added dataset.csv

--- prediction.py
+++ prediction.py
@@ -5,3 +5,9 @@ import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+
+urls = pd.read_csv('dataset.csv')
+
+# print(type(urls))
+
+print(urls.head())
