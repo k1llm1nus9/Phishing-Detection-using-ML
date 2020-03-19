3501dc2089b89c2cb5924b89e02fba34f37285d4
jyotishree <jyotishree1696@gmail.com>
2020-03-19 15:42:38 +0530

    started with prediction model

---
+++ prediction.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import random
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
